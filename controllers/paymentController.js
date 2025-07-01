5c78b5c744548e175911880960bc58e84354ad3b
npeterson1004 <npeterson1004@gmail.com>
2025-07-01 18:14:10 -0500

    adding delivery method

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -19,7 +19,8 @@ exports.processVenmoPayment = async (req, res) => {
         res.status(200).json({
             message: "Payment request sent. Complete payment in Venmo.",
             venmoUsername: venmoUsername,
-            orderNumber: order.orderNumber // ✅ Return readable order number
+            orderNumber: order.orderNumber, // ✅ Return readable order numb
+            totalAmount: order.totalAmounter
         });
         
     } catch (error) {
